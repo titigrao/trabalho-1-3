a716749e7d25b5a40c6633d7407c30a2f3535d2b
Titigas <127964804+titigrao@users.noreply.github.com>
2024-10-02 09:36:50 -0300

    nh

--- main.js
+++ main.js
@@ -14,3 +14,13 @@ async function visualizarDados(){
 visualizarDados()
 
 
+const paragrafo = document.createElement('p')
+
+paragrafo.classList.add('graficos--container__texto')
+
+paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
+
+
+const container = document.getElementById('graficos--container')
+
+container.appendChild(paragrafo)
