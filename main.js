915e48876020150791ae9e8ab3ebfffe513184af
Titigas <127964804+titigrao@users.noreply.github.com>
2024-10-02 09:37:48 -0300

    .

--- main.js
+++ main.js
@@ -10,17 +10,19 @@ async function visualizarDados(){
    resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
   resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
   console.log(resultado_dados, resultado_regiao)
+
+  const paragrafo = document.createElement('p')
+
+  paragrafo.classList.add('graficos--container__texto')
+  
+  paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
+  
+  
+  const container = document.getElementById('graficos--container')
+  
+  container.appendChild(paragrafo)
+
 }
 visualizarDados()
 
 
-const paragrafo = document.createElement('p')
-
-paragrafo.classList.add('graficos--container__texto')
-
-paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
-
-
-const container = document.getElementById('graficos--container')
-
-container.appendChild(paragrafo)
