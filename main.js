7253534f94b8a9fe6962764d8238da474e0194d3
Titigas <127964804+titigrao@users.noreply.github.com>
2024-10-23 09:28:13 -0300

    .

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ async function visualizarDados(){
   const dados = await res.json()
   regiao = 0
     tipo_dados = (dados[0]['variavel'])
-  console.log(tipo_dados)
+  console.log(Object.values(tipo_dados))
 
    resultado_dados = (dados[0]['resultados'][0]["series"][regiao]['serie'])
   resultado_regiao = (dados[0]['resultados'][0]["series"][regiao]["localidade"]['nome'])
+  let ano = Object.keys(resultado_dados)
+  let valor = Object.values(resultado_dados)
+  console.log(ano[0], valor[0])
   console.log(resultado_dados, resultado_regiao)
 
   const paragrafo = document.createElement('p')
-
   paragrafo.classList.add('graficos--container__texto')  
   paragrafo.innerHTML = `Está é uma pesquisa do IBGE com o foco em ${tipo_dados}`
-  console.log(paragrafo)  
+
   const container = document.getElementById('graficos--container')
   
   container.appendChild(paragrafo)
